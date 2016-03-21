9ef8c7b0a3da3786a07aaabdebbee2da589995c8
sacreman <sacreman@gmail.com>
2016-03-21 11:03:17 +0000

    fix up test utils

--- test/listen.py
+++ test/listen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import socket
 import binascii
+from time import sleep
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5555
@@ -9,16 +10,16 @@ BUFFER_SIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+conn, addr = s.accept()
 
 while True:
     try:
-        conn, addr = s.accept()
         packet = conn.recv(BUFFER_SIZE)
         if not packet:
             continue
         print "received data:", binascii.hexlify(packet)
-        conn.send("hello")
-    except Exception as e:
+        sleep(1)
+    except Exception, e:
         print e
         break
 
